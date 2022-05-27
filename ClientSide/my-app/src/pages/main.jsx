457b6d6e39bcefbfb6b2c52565e24fd954068ed5
Abu Bakar <abubakr@miletap.com>
2022-05-28 02:51:41 +0500

    Add Clientside

---
+++ ClientSide/my-app/src/pages/main.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Main() {
+    function download(blob, filename) {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        // the filename you want
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }
+    const handleChange = (event) => {
+        const myFile = event.target.files[0];
+        const data = new FormData()
+        data.append('somefile', myFile)
+        fetch(`http://192.53.175.240:4000/api/excetToPdfConvert`, {
+            method: "POST",
+            body: data,
+            // headers: {
+            //     'Content-Type': 'application/json'
+            // }
+        }).then((res) => {
+            return res.blob()
+        })
+            .then((blob) => download(blob, "Result"))
+    }
+
+    return (
+        <div>
+            <h1 className="companyTitle" >Welcome <br />S.R. Laboratories</h1>
+            <div>
+                <div className="inputCont" >
+                    <div className="inputItem excel" >
+                        Excel To Pdf
+                        <input type="file" onChange={(event) => handleChange(event)}
+                            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+                       
+                    </div>
+                    <div className="inputItem word" >
+                        Word To Pdf
+                        <input type="file" onChange={(event) => handleChange(event)} 
+                        accept="application/msword, .doc, .docx"/>
+                    </div>
+                    <div className="inputItem ppt" >
+                    Powerpoint To Pdf
+                        <input type="file" onChange={(event) => handleChange(event)} 
+                        accept="application/vnd.ms-powerpoint, .ppt, .pptx"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Main
